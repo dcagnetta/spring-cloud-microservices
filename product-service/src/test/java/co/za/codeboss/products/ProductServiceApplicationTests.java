ad3f32e6354b76d159577e4fd7fc5ee31ba7e66a
Dillan Cagnetta <dillancagnetta@yahoo.com>
2020-04-17 05:31:22 +0200

    Added: product service

---
+++ product-service/src/test/java/co/za/codeboss/products/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.codeboss.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
